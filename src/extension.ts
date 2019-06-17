1d4f54833bbdbb192a85b447bd3e9df7eece5655
James Foster <github@jgfoster.net>
2019-06-17 07:18:49 -0700

    Use an outputChannel to log GemStone activity

--- src/extension.ts
+++ src/extension.ts
@@ -11,8 +11,11 @@ const { GciSession } = require('gci-js');
 
 const config = vscode.workspace.getConfiguration('gemstone');
 var sessions: Session[] = [];
+var outputChannel: vscode.OutputChannel;
 
 export function activate(context: vscode.ExtensionContext) {
+	outputChannel = vscode.window.createOutputChannel('GemStone');
+	outputChannel.appendLine('Activated GemStone extension');
 	const loginsProvider = new LoginsProvider();
 	vscode.window.registerTreeDataProvider('gemstone-logins', loginsProvider);
 	const sessionsProvider = new SessionsProvider(sessions);
@@ -23,10 +26,12 @@ export function activate(context: vscode.ExtensionContext) {
 		const session = new Session(login, gciSession);
 		sessions.push(session);
 		sessionsProvider.refresh();
+		outputChannel.appendLine('Login ' + session.description);
 	});
 	context.subscriptions.push(loginCommand);
 
 	const sessionCommand = vscode.commands.registerCommand('gemstone.logout', (session: Session) => {
+		outputChannel.appendLine('Logout ' + session.description);
 		session.logout();
 		const index = sessions.indexOf(session);
 		sessions.splice(index, 1);
@@ -36,4 +41,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	outputChannel.appendLine('Deactivated GemStone extension');
+}
